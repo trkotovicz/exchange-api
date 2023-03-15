b7b462e7a0eb1b35bb0a91f945484837a2d5df3c
Thais Kotovicz <thais_kotovicz@hotmail.com>
2023-03-15 16:43:43 -0300

    test: login integration tests

--- tests/integration/user.test.ts
+++ tests/integration/user.test.ts
@@ -1,14 +1,21 @@
-import { expect } from 'chai';
-import sinon from 'sinon';
+import chai from 'chai';
+import sinon, { SinonSandbox } from 'sinon';
+import chaiHttp from 'chai-http';
 import request from 'supertest';
 import app from '../../src/app';
 import User from '../../src/models/User';
 import UserService from '../../src/services/User';
 import { userMock } from '../mocks/userMocks';
+import JwtService from '../../src/services/jwtService';
+import { ErrorTypes } from '../../src/errors/catalog';
 
-describe('userService', () => {
+const { expect } = chai;
 
-  describe('createUser', () => {
+chai.use(chaiHttp);
+
+describe('User integration tests', () => {
+
+  describe('POST /user', () => {
 
     describe('on success', () => {
       let stub: sinon.SinonSpy<any, any>;
@@ -51,5 +58,45 @@ describe('userService', () => {
         existingUserStub.restore(); 
       });
     });
-  })
-})
+  });
+
+  describe ('POST /login', () => {
+    const userService = new UserService();
+    let sandbox: SinonSandbox;
+
+    beforeEach(() => sandbox = sinon.createSandbox());
+  
+    afterEach(() => sandbox.restore());
+
+    it('should return a valid JWT Token', async () => {
+      const username = 'mocked_user';
+      const password = 'mocked_password';
+      const mockUserData = { id: 1, username: 'mocked_user' };
+
+      const userServiceLoginMock = sandbox.stub(userService, 'login').resolves(mockUserData);
+      const jwtServiceCreateTokenSpy = sandbox.spy(JwtService, 'createToken');
+
+      const res = await request(app)
+        .post('/login')
+        .send({ username, password });
+
+      expect(res.status).to.equal(200);
+      expect(res.body).to.have.property('user');
+      expect(res.body.user).to.deep.equal(mockUserData);
+      expect(res.body).to.have.property('token');
+
+      sinon.assert.calledWith(jwtServiceCreateTokenSpy, { id: 1, username: 'mocked_user' });
+    });
+
+    it('should return 401 if login fails', async () => {
+      sandbox.stub(userService, 'login').throws(new Error(ErrorTypes.UnauthorizedError));
+
+      const res = await request(app)
+        .post('/login')
+        .send({ username: 'mocked_user', password: 'wrong_password' });
+      
+        expect(res.status).to.equal(401);
+        expect(res.body.error).to.equal('Invalid username or password');
+    });
+  });
+});
